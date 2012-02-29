704b65a0341df24985c687dba9151768c0cb9b9b
gregcaporaso <gregcaporaso@gmail.com>
2012-02-29 06:03:46 +0000

    added __version__, etc, so it's easy to pull out in places like print_qiime_config.py

--- python-code/biom/__init__.py
+++ python-code/biom/__init__.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 
 __all__ = ['table','parse','unittest']
+__author__ = "Greg Caporaso, Daniel McDonald"
+__copyright__ = "Copyright 2012, BIOM-Format Project"
+__credits__ = ["Daniel McDonald", "Greg Caporaso"]
+__license__ = "GPL"
+__url__ = "http://biom-format.org"
+__version__ = "0.9dev"
+__maintainer__ = "Daniel McDonald"
+__email__ = "daniel.mcdonald@colorado.edu"
+__status__ = "Development"
