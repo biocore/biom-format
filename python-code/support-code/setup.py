f5dbf206c5b58ac7a1333354b34f264863e6c515
Greg Caporaso <gregcaporaso@gmail.com>
2012-05-15 06:49:15 -0700

    cleaned up and fixed bug

--- python-code/support-code/setup.py
+++ python-code/support-code/setup.py
@@ -1,19 +1,20 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-import os
-import numpy
+from os.path import join, split
+from os import getcwd
+try:
+    import numpy
+except ImportError:
+    raise ImportError, "numpy cannot be found. Can't continue."
 
-# Compiling Pyrex modules to .c and .so
-include_path = os.path.join(os.getcwd(), 'include')
-# find arrayobject.h on every system an alternative would be to put
-# arrayobject.h into pycogent/include, but why .. 
-library_path = os.path.split(numpy.__file__)[0]
+include_path = join(getcwd(), 'include')
+library_path = split(numpy.__file__)[0]
 
 setup(ext_modules=[Extension(
                    "_sparsemat",                 # name of extension
                    ["sparsemat.pyx", "sparsemat_lib.cpp"], #  our Cython source
                    language="c++",  # causes Cython to create C++ source
-                   library_dirs=library_path,
-                   include_dirs=include_path)],
+                   library_dirs=[library_path],
+                   include_dirs=[include_path])],
       cmdclass={'build_ext': build_ext})
