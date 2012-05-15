4740a25e2b08968926013494269ddc7a3618cc58
Greg Caporaso <gregcaporaso@gmail.com>
2012-05-15 06:45:17 -0700

    modified to remove absolute paths in include and library paths

--- python-code/support-code/setup.py
+++ python-code/support-code/setup.py
@@ -1,11 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
+import numpy
+
+# Compiling Pyrex modules to .c and .so
+include_path = os.path.join(os.getcwd(), 'include')
+# find arrayobject.h on every system an alternative would be to put
+# arrayobject.h into pycogent/include, but why .. 
+library_path = os.path.split(numpy.__file__)[0]
 
 setup(ext_modules=[Extension(
                    "_sparsemat",                 # name of extension
                    ["sparsemat.pyx", "sparsemat_lib.cpp"], #  our Cython source
                    language="c++",  # causes Cython to create C++ source
-                   library_dirs=['/Users/mcdonald/lib'],
-                   include_dirs=['/Users/mcdonald/include/python2.7'])],
+                   library_dirs=library_path,
+                   include_dirs=include_path)],
       cmdclass={'build_ext': build_ext})
