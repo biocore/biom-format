4b92caa266d7236edc93d488b82a47d7761cae6e
Daniel McDonald <mcdonadt@colorado.edu>
2012-05-15 07:05:43 -0600

    added ifdef for debug

--- python-code/support-code/sparsemat_lib.cpp
+++ python-code/support-code/sparsemat_lib.cpp
@@ -7,12 +7,22 @@ using namespace sparsemat;
 SparseMatFloat::SparseMatFloat() {}
 
 void SparseMatFloat::insert(uint32_t row, uint32_t col, double value) {
-        current_key = MAKE_KEY(row,col);
-        hash[current_key] = value;
+    current_key = MAKE_KEY(row,col);
+#ifdef _SPARSEMAT_LIB_DEBUG
+    std::cout << "row << 30: " << (row << 30) << " row << 31: " << (row << 31);
+    std::cout << " row << 32: " << (row << 32) << std::endl; 
+    std::cout << "Row: " << row << "\tCol: " << col << std::endl;
+    std::cout << "Current key: " << current_key << "\tvalue: " << value << std::endl;
+#endif
+    hash[current_key] = value;
 }
 
 double SparseMatFloat::get(uint32_t row, uint32_t col) {
     current_key = MAKE_KEY(row, col);
+#ifdef _SPARSEMAT_LIB_DEBUG
+    std::cout << "Row: " << row << "\tCol: " << col << std::endl;
+    std::cout << "Current key: " << current_key << std::endl;
+#endif
     return hash[current_key];
 }
 
@@ -20,12 +30,20 @@ double SparseMatFloat::get(uint32_t row, uint32_t col) {
 SparseMatInt::SparseMatInt() {}
 
 void SparseMatInt::insert(uint32_t row, uint32_t col, int value) {
-        current_key = MAKE_KEY(row,col);
-        hash[current_key] = value;
+    current_key = MAKE_KEY(row,col);
+#ifdef _SPARSEMAT_LIB_DEBUG
+    std::cout << "Row: " << row << "\tCol: " << col << std::endl;
+    std::cout << "Current key: " << current_key << "\tvalue: " << value << std::endl;
+#endif
+    hash[current_key] = value;
 }
 
 int SparseMatInt::get(uint32_t row, uint32_t col) {
     current_key = MAKE_KEY(row, col);
+#ifdef _SPARSEMAT_LIB_DEBUG
+    std::cout << "Row: " << row << "\tCol: " << col << std::endl;
+    std::cout << "Current key: " << current_key << std::endl;
+#endif
     return hash[current_key];
 }
 
