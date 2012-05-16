12d306ecb73befd059c994be70f8d6be26ff1031
Daniel McDonald <mcdonadt@colorado.edu>
2012-05-16 11:51:09 -0600

    added doc strings to cython objects

--- python-code/support-code/sparsemat.pyx
+++ python-code/support-code/sparsemat.pyx
@@ -1,17 +1,21 @@
 # to int or to uint...
+
 cdef extern from "sparsemat_lib.h" namespace "sparsemat":
-    struct items_int:
+    cdef struct items_int:
+        # support struct for returning all rows/cols/values from a integer mat
         int *rows
         int *cols
         int *values
-    
-    struct items_float:
+
+    cdef struct items_float:
+        # support struct for returning all rows/cols/values from a double 
+        # precision floating point mat    
         int *rows
         int *cols
         double *values
-        
-cdef extern from "sparsemat_lib.h" namespace "sparsemat":
+    
     cdef cppclass SparseMatFloat:
+        # prototype for SparseMatFloat c++ object
         SparseMatFloat()
         void insert(int, int, double)
         double get(int, int)
@@ -22,6 +26,7 @@ cdef extern from "sparsemat_lib.h" namespace "sparsemat":
         items_float items()
         
     cdef cppclass SparseMatInt:
+        # prototype for SparseMatInt c++ object
         SparseMatInt()
         void insert(int, int, int)
         int get(int, int)
@@ -31,42 +36,54 @@ cdef extern from "sparsemat_lib.h" namespace "sparsemat":
         items_int keys()
         items_int items()
         
-# should really use inheritence here but things were getting odd with *thisptr
-# ...and i dont care right now
-
+# inheritence was not working as expected, so it was scrapped.
 cdef class PySparseMatFloat:
-    cdef SparseMatFloat *thisptr
+    # provide a python interface into the c++ object
+    cdef SparseMatFloat *thisptr # so we can refer to self
+    
     def __cinit__(self):
+        """Using default constructor"""
         self.thisptr = new SparseMatFloat()
 
     def __dealloc__(self):
+        """Using default destructor"""
         del self.thisptr
 
+    def __str__(self):
+        """Output like a python dict"""
+        output = []
+        for (r,c),v in self.items():
+            output.append('(%d, %d):%f' % (r,c,v))
+        output = ', '.join(output)
+        return '{%s}' % output
+        
     def insert(self, row, col, value):
+        """Insert a value into the matrix"""
         self.thisptr.insert(<unsigned int>row, <unsigned int>col, value)
    
     def get(self, row, col):
-        cdef float foo
-        cdef double bar
-        foo = self.thisptr.get(<unsigned int>row, <unsigned int>col)
-        bar = self.thisptr.get(<unsigned int>row, <unsigned int>col)
-
-        print "foo: ", foo
-        print "bar: ", bar
-        
+        """Get a value from the matrix, return 0.0 if doesn't exist"""
         return self.thisptr.get(<unsigned int>row, <unsigned int>col)
 
     def erase(self, row, col):
+        """Delete a value from the matrix"""
         self.thisptr.erase(<unsigned int>row, <unsigned int>col)
         
     def contains(self, row, col):
-        return self.thisptr.contains(<unsigned int>row, <unsigned int>col)
-    
+        """Return True if the row/col exists, False otherwise"""
+        if self.thisptr.contains(<unsigned int>row, <unsigned int>col) == 1:
+            return True
+        else:
+            return False
+        
     def length(self):
+        """Return the number of elements within the matrix"""
         return self.thisptr.length()
         
     def keys(self):
+        """Return keys [(row,col)] similar to Python dict.keys()"""
         cdef items_float foo
+        cdef Py_ssize_t i
         foo = self.thisptr.keys()
         
         results = []
@@ -76,7 +93,9 @@ cdef class PySparseMatFloat:
         return results
     
     def items(self):
+        """Return items [((row,col),value)] similar to Python dict.items()"""
         cdef items_float foo
+        cdef Py_ssize_t i
         foo = self.thisptr.items()
         
         results = []
@@ -86,29 +105,50 @@ cdef class PySparseMatFloat:
         return results
     
 cdef class PySparseMatInt:
-    cdef SparseMatInt *thisptr
+    # provide a python interface into the c++ object
+    cdef SparseMatInt *thisptr # so we can refer to self
+    
     def __cinit__(self):
+        """Using default constructor"""
         self.thisptr = new SparseMatInt()
 
     def __dealloc__(self):
+        """Using default destructor"""
         del self.thisptr
+
+    def __str__(self):
+        """Output like a python dict"""
+        output = []
+        for (r,c),v in self.items():
+            output.append('(%d, %d):%d' % (r,c,v))
+        output = ', '.join(output)
+        return '{%s}' % output
    
     def insert(self, row, col, value):
+        """Insert a value into the matrix"""
         self.thisptr.insert(<unsigned int>row, <unsigned int>col, value)
    
     def get(self, row, col):
+        """Get a value from the matrix, return 0.0 if doesn't exist"""
         return self.thisptr.get(<unsigned int>row, <unsigned int>col)
 
     def erase(self, row, col):
+        """Delete a value from the matrix"""
         self.thisptr.erase(<unsigned int>row, <unsigned int>col)
 
     def contains(self, row, col):
-        return self.thisptr.contains(<unsigned int>row, <unsigned int>col)
-
+        """Return True if the row/col exists, False otherwise"""
+        if self.thisptr.contains(<unsigned int>row, <unsigned int>col) == 1:
+            return True
+        else:
+            return False
+            
     def length(self):
+        """Return the number of elements within the matrix"""
         return self.thisptr.length()
         
     def keys(self):
+        """Return keys [(row,col)] similar to Python dict.keys()"""
         cdef items_int foo
         foo = self.thisptr.keys()
         
@@ -119,6 +159,7 @@ cdef class PySparseMatInt:
         return results
 
     def items(self):
+        """Return items [((row,col),value)] similar to Python dict.items()"""
         cdef items_int foo
         foo = self.thisptr.items()
         
