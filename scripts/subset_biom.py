5582b7f828b3ed0876d98de17faa8f1168c2e7e1
Jai Ram Rideout <jai.rideout@gmail.com>
2013-01-28 16:17:09 -0700

    Updated version strings to 1.1.2.

--- scripts/subset_biom.py
+++ scripts/subset_biom.py
@@ -7,7 +7,7 @@ __copyright__ = "Copyright 2012, The BIOM-Format Project"
 __credits__ = ["Daniel McDonald"]
 __url__ = "http://biom-format.org"
 __license__ = "GPL"
-__version__ = "1.1.1-dev"
+__version__ = "1.1.2"
 __maintainer__ = "Daniel McDonald"
 __email__ = "daniel.mcdonald@colorado.edu"
 
