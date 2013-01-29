3268da04aed6696873753e979e5e51c876f60184
Jai Ram Rideout <jai.rideout@gmail.com>
2013-01-28 17:00:09 -0700

    Updated version strings from 1.1.2 to 1.1.2-dev.

--- python-code/tests/test_util.py
+++ python-code/tests/test_util.py
@@ -15,7 +15,7 @@ __credits__ = ["Rob Knight", "Peter Maxwell", "Sandra Smit",
                "Jai Ram Rideout"]
 __license__ = "GPL"
 __url__ = "http://biom-format.org"
-__version__ = "1.1.2"
+__version__ = "1.1.2-dev"
 __maintainer__ = "Daniel McDonald"
 __email__ = "daniel.mcdonald@colorado.edu"
 
