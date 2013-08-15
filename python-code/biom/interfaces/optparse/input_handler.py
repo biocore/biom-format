d15a6a0ff547104452245b55b35211a785ac060f
Greg Caporaso <gregcaporaso@gmail.com>
2013-08-15 12:20:21 -0700

    updated based on suggestions from @jrrideout

--- python-code/biom/interfaces/optparse/input_handler.py
+++ python-code/biom/interfaces/optparse/input_handler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+from biom.util import biom_open
 from biom.parse import parse_biom_table
 
 def load_biom_table(biom_fp):
-    return parse_biom_table(open(biom_fp,'U'))
+    with biom_open(biom_fp, 'U') as table_f:
+        return parse_biom_table(table_f)
