18aece0367ae3d196d5432b3684cc586932d75da
Greg Caporaso <gregcaporaso@gmail.com>
2013-08-15 13:26:34 -0700

    added header information

--- python-code/biom/interfaces/optparse/input_handler.py
+++ python-code/biom/interfaces/optparse/input_handler.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+__author__ = "Greg Caporaso"
+__copyright__ = "Copyright 2013, The BIOM-Format project"
+__credits__ = ["Greg Caporaso"]
+__license__ = "GPL"
+__version__ = "1.1.2-dev"
+__maintainer__ = "Greg Caporaso"
+__email__ = "gregcaporaso@gmail.com"
+
 from biom.util import biom_open
 from biom.parse import parse_biom_table
 
