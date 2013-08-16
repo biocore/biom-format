f33c23cfe6cb5170e6d2ab75b540402f80f25943
Greg Caporaso <gregcaporaso@gmail.com>
2013-08-16 13:51:06 -0700

    updated to use make_parameter_collection_lookup_f

--- python-code/biom/interfaces/optparse/config/print_biom_table_summary.py
+++ python-code/biom/interfaces/optparse/config/print_biom_table_summary.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
+__author__ = "Greg Caporaso"
+__copyright__ = "Copyright 2013, The BIOM-Format project"
+__credits__ = ["Greg Caporaso"]
+__license__ = "GPL"
+__version__ = "1.1.2-dev"
+__maintainer__ = "Greg Caporaso"
+__email__ = "gregcaporaso@gmail.com"
+
+from pyqi.core.command import make_parameter_collection_lookup_f
+from pyqi.core.interfaces.optparse.output_handler import write_list_of_strings
 from pyqi.core.interfaces.optparse import (OptparseOption,
                                            OptparseUsageExample,
                                            OptparseOption, OptparseResult)
-from pyqi.core.interfaces.optparse.output_handler import write_list_of_strings
 from biom.commands.table_summarizer import CommandConstructor
 from biom.interfaces.optparse.input_handler import load_biom_table
+
+param_lookup = make_parameter_collection_lookup_f(CommandConstructor)
 
 usage_examples = [
     OptparseUsageExample(ShortDesc="Basic script usage",
@@ -14,12 +25,12 @@ usage_examples = [
 ]
 
 inputs = [
-    OptparseOption(Parameter=CommandConstructor.Parameters['table'],
+    OptparseOption(Parameter=param_lookup('table'),
                    InputType="existing_filepath",
                    InputHandler=load_biom_table,
                    ShortName='i',
                    Name='input_fp'),
-    OptparseOption(Parameter=CommandConstructor.Parameters['qualitative'],
+    OptparseOption(Parameter=param_lookup('qualitative'),
                    InputType=None,
                    InputAction="store_true",
                    InputHandler=None,
