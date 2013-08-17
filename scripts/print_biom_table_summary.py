950923c6a41b3ceb769a84df696f7f11fb6558de
Jai Ram Rideout <jai.rideout@gmail.com>
2013-08-17 00:10:07 -0400

    Fix copyright, author, and version info

--- scripts/print_biom_table_summary.py
+++ scripts/print_biom_table_summary.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 __author__ = "Greg Caporaso"
-__copyright__ = "Copyright 2013, The QIIME Project"
-__credits__ = ["Greg Caporaso", "Daniel McDonald", "Doug Wendel",
-               "Jai Ram Rideout"]
+__copyright__ = "Copyright 2013, The BIOM-Format project"
+__credits__ = ["Greg Caporaso", "Jai Ram Rideout"]
 __license__ = "GPL"
-__version__ = "0.1.0-dev"
+__version__ = "1.1.2-dev"
 __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 
