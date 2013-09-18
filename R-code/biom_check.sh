9abb420e22591d9f22058bafcfad1ba512bf85ba
Paul J. McMurdie <mcmurdie@stanford.edu>
2013-09-18 10:39:20 -0700

    Removed R-code subdir, and updated index.rst with links.
    This contributes to [Issue
    211](https://github.com/biom-format/biom-format/issues/211).
    
    https://github.com/biom-format/biom-format/issues/211

--- R-code/biom_check.sh
+++
@@ -1,5 +0,0 @@
-# This runs the full-throated R-lang complete package system check,
-# which also checks for code issues, missing dependencies, and other
-# important issues. Will take much longer to run than the "unit tests only" script.
-# Note that this will also run the unit tests though...
-R CMD check biom
