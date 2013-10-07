17cee4a1f7976fb96d814dae0e346530ebb3dcda
Jai Ram Rideout <jai.rideout@gmail.com>
2013-10-07 14:07:18 -0400

    Add results fp option to bench.sh
    Also reports errors when running bench scripts

--- python-code/tests/bench/bench.sh
+++ python-code/tests/bench/bench.sh
@@ -4,14 +4,12 @@
 # Copyright 2012, BIOM Format
 # This code is licensed under the GPL
 
-#TIME_CMD='/usr/bin/time -f "%M %e"' # doesn't work on OS X
-TIME_CMD='time'
-
 SCRIPTS_DIR=$1
 TABLES_DIR=$2
+RESULTS_FP=$3
 
 usage () {
-    echo "usage: bench.sh <path_to_scripts> <path_to_tables>"
+    echo "usage: bench.sh <path_to_scripts> <path_to_tables> <path_to_results>"
     exit 0;
 }
 
@@ -25,17 +23,22 @@ then
     usage ;
 fi
 
+if [ -e $RESULTS_FP ]
+then
+    rm $RESULTS_FP
+fi
+
 for f in `/bin/ls $SCRIPTS_DIR | grep "\.py$"`
 do
     for t in `/bin/ls $TABLES_DIR`
     do
-        $TIME_CMD python $SCRIPTS_DIR/$f $TABLES_DIR/$t 2> /tmp/foo
+        /usr/bin/time -f "%M\t%e" python $SCRIPTS_DIR/$f $TABLES_DIR/$t 2> /tmp/foo
         if [ $? -ne 0 ]
         then
-            continue
+            echo "$f\t$t\tERROR" >> $RESULTS_FP
         else
             foo=`cat /tmp/foo`
-            echo "$f\t$t\t$foo" >> bench_results.txt
+            echo "$f\t$t\t$foo" >> $RESULTS_FP
         fi
     done
 done
