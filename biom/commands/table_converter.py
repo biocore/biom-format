216c8960b49b30d982efa9beac81c2591e2c0519
Jai Ram Rideout <jai.rideout@gmail.com>
2013-10-14 11:33:01 -0400

    Add header to biom/ source files

--- biom/commands/table_converter.py
+++ biom/commands/table_converter.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+#-----------------------------------------------------------------------------
+# Copyright (c) 2011-2013, The BIOM Format Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 from __future__ import division
 from pyqi.core.command import Command, Parameter, ParameterCollection
 from pyqi.core.exception import CommandError
--- biom/table.py
+++ biom/table.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-
 """Core BIOM objects for dense and sparse tables"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2011-2013, The BIOM Format Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
 from __future__ import division
 
