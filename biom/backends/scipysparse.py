25d2086638efa1629bda261284f51b984a7d11ac
Jai Ram Rideout <jai.rideout@gmail.com>
2013-10-17 13:08:20 -0400

    Update header with license

--- biom/backends/scipysparse.py
+++ biom/backends/scipysparse.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2011-2013, The BIOM Format Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 from __future__ import division
 
 __author__ = "Jai Ram Rideout"
-__copyright__ = "Copyright 2013, BIOM-Format Project"
+__copyright__ = "Copyright 2011-2013, The BIOM Format Development Team"
 __credits__ = ["Jai Ram Rideout", "Daniel McDonald"]
-__license__ = "GPL"
+__license__ = "BSD"
 __url__ = "http://biom-format.org"
 __version__ = "1.2.0-dev"
 __maintainer__ = "Jai Ram Rideout"
