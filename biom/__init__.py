500a412cd353d18830490ad5d4252bba3d1ac514
Daniel McDonald <mcdonadt@colorado.edu>
2014-04-22 19:00:06 -0600

    ENH: more contribs :)

--- biom/__init__.py
+++ biom/__init__.py
@@ -11,7 +11,9 @@
 __author__ = "Daniel McDonald"
 __copyright__ = "Copyright 2011-2013, The BIOM Format Development Team"
 __credits__ = ["Daniel McDonald", "Jai Ram Rideout", "Greg Caporaso",
-               "Jose Clemente", "Justin Kuczynski"]
+               "Jose Clemente", "Justin Kuczynski", "Antonio Gonzalez",
+               "Yoshiki Vazquez Baeza", "Jose Navas", "Adam Robbins-Pianka",
+               "Rob Knight", "Joshua Shorenstein", "Emily TerAvest"]
 __license__ = "BSD"
 __url__ = "http://biom-format.org"
 __version__ = "1.3.1-dev"
