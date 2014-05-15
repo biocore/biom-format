8d621472847a279767513ba7762de50c631fe687
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-05-15 13:25:16 -0600

    DOC: Remove outdated documentation

--- biom/table.py
+++ biom/table.py
@@ -8,22 +8,6 @@ The biom-format project provides rich ``Table`` objects to support use of the
 BIOM file format. The objects encapsulate matrix data (such as OTU counts) and
 abstract the interaction away from the programmer.
 
-
-Abstract base classes
----------------------
-
-Abstract base classes establish standard interfaces for subclassed types and
-provide common functionality for derived types. 
-
-``Table`` is a container object and an abstract base class that provides a
-common and required API for subclassed objects. Through the use of private
-interfaces, it is possible to create public methods that operate on the
-underlying datatype without having to implement each method in each subclass.
-For instance, ``Table.iter_data`` will return a generator that always yields
-``numpy.array`` vectors for each sample regardless of how the table data is
-actually stored. This functionality results from derived classes implementing
-private interfaces, such as ``Table._to_dense``.
-
 .. currentmodule:: biom.table
 
 Classes
