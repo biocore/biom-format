7e093d29113353aa4f10a6677a9063dff6cba2ba
Daniel McDonald <mcdonadt@colorado.edu>
2014-05-15 16:20:27 -0600

    DOC: module level ex starting

--- biom/table.py
+++ biom/table.py
@@ -18,6 +18,35 @@ Classes
 
    Table
 
+Examples
+--------
+First, lets create a toy table to play around with. For this example, we're going to construct a 10x4 `Table`, or one that has 10 observations and 4 samples. Each observation and sample will be given an arbitrary but unique name. We'll also add on some metadata.
+
+>>> import numpy as np
+>>> from biom.table import Table
+>>> data = np.arange(40).reshape(10, 4)
+>>> sample_ids = ['S%d' % i for i in range(4)]
+>>> observ_ids = ['O%d' % i for i in range(10)]
+>>> sample_metadata = [{'environment': 'A'}, {'environment': 'B'},
+...                    {'environment': 'A'}, {'environment': 'B'}]
+>>> observ_metadata = [{'taxonomy': ['Bacteria', 'Firmicutes']},
+...                    {'taxonomy': ['Bacteria', 'Firmicutes']},
+...                    {'taxonomy': ['Bacteria', 'Proteobacteria']},
+...                    {'taxonomy': ['Bacteria', 'Proteobacteria']},
+...                    {'taxonomy': ['Bacteria', 'Proteobacteria']},
+...                    {'taxonomy': ['Bacteria', 'Bacteroidetes']},
+...                    {'taxonomy': ['Bacteria', 'Bacteroidetes']},
+...                    {'taxonomy': ['Bacteria', 'Firmicutes']},
+...                    {'taxonomy': ['Bacteria', 'Firmicutes']},
+...                    {'taxonomy': ['Bacteria', 'Firmicutes']}]
+>>> table = Table(data, observ_ids, sample_ids, observ_metadata,
+...               sample_metadata)
+
+Now that we have a table, let's explore a few of the member variables that are
+provided by the object.
+
+>>> table.sample_ids
+
 """
 
 # -----------------------------------------------------------------------------
