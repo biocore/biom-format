0a74b36e7778c7a7aef89d33558d0a05dd24c676
Daniel McDonald <mcdonadt@colorado.edu>
2014-08-05 00:11:04 -0600

    API: error profile support

--- biom/err.py
+++ biom/err.py
@@ -1,3 +1,63 @@
+#!/usr/bin/env python
+
+r"""Error profile
+
+The error profile object to define different error types and handling within
+BIOM. The following types are registered with the associated default states
+
+empty : 'ignore'
+    Treatment of an empty table (e.g., Table.sum(axis='whole') == 0). If a
+    callable is provided, it implies 'call' and will set the callback function.
+
+obssize : 'raise'
+    Treatment of a table in which the number of observation ids does not match
+    the size of the data.
+
+sampsize : 'raise'
+    Treatment of a table in which the number of sample ids does not match the
+    size of the data.
+
+obsdup : 'raise'
+    Treatment of duplicate observation IDs.
+
+sampdup : 'raise'
+    Treatment of duplicate sample IDs.
+
+obsmdsize : 'raise'
+    Treatment of a table in which the number of observation metadata elements
+    differs from the size of the data.
+
+sampmdsize : 'raise'
+    Treatment of a table in which the number of sample metadata elements
+    differs from the size of the data.
+
+Examples
+--------
+
+Use `seterr` to change how empty tables are handled:
+
+>>> from numpy import inf
+>>> from biom.err import seterr
+>>> from biom import example_table
+>>> make_empty_f = lambda v, i, md: v.sum() > inf
+>>> _ = example_table.filter(make_empty_f, inplace=False)
+>>> old_state = seterr(empty='raise')
+>>> example_table.filter(make_empty_f, inplace=False)
+Traceback (most recent call last):
+...
+TableException: Empty table!
+>>> _ = seterr(**old_state)
+
+"""
+
+# -----------------------------------------------------------------------------
+# Copyright (c) 2011-2013, The BIOM Format Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# -----------------------------------------------------------------------------
+
 from warnings import warn
 from sys import stdout
 from contextlib import contextmanager
@@ -6,23 +66,144 @@ import types
 from biom.exception import TableException
 
 
-def _raise_ex(ex):
-    """Raise an exception, useful for raising from lambdas"""
-    raise ex
+# error messages
+EMPTY = "Empty table!"
+OBSSIZE = "Number of observation IDs differs from matrix size!"
+SAMPSIZE = "Number of sample IDs differs from matrix size!"
+OBSDUP = "Duplicate observation IDs"
+SAMPDUP = "Duplicate sample IDs!"
+OBSMDSIZE = "Size of observation metadata differs from matrix size!"
+SAMPMDSIZE = "Size of sample metadata differs from matrix size!"
 
 
-class _ErrorProfile(object):
+def _test_empty(t):
+    """Check if t is empty"""
+    return t.is_empty()
+
+
+def _test_obssize(t):
+    """Check if the number of observations match data size"""
+    return t.shape[0] != len(t.ids(axis='observation'))
+
+
+def _test_sampsize(t):
+    """Check if the number of samples match data size"""
+    return t.shape[1] != len(t.ids(axis='sample'))
+
+
+def _test_obsdup(t):
+    """Check if there are duplicate observations"""
+    return t.shape[0] != len(set(t.ids(axis='observation')))
+
+
+def _test_sampdup(t):
+    """Check if there are duplicate samples"""
+    return t.shape[1] != len(set(t.ids(axis='sample')))
+
+
+def _test_obsmdsize(t):
+    """Check if the size of the observation metadata matches data size"""
+    md = t.metadata(axis='observation')
+    return t.shape[0] != len(md) if md is not None else False
+
+
+def _test_sampmdsize(t):
+    """Check if the size of the sample metadata matches data size"""
+    md = t.metadata(axis='sample')
+    return t.shape[1] != len(md) if md is not None else False
+
+
+def _create_error_states(msg, callback, exception):
+    """Create an error states"""
+    return {'ignore': lambda x: None,
+            'warn': lambda x: warn(msg),
+            'raise': lambda x: exception(msg),
+            'call': callback if callback is not None else lambda x: None,
+            'print': lambda x: stdout.write(msg + '\n')}
+
+
+class ErrorProfile(object):
+    """An error profile
+
+    The error profile defines the types of errors that can be optionally
+    handled, how those errors are handled, and the actual handling of the
+    errors.
+    """
     _valid_states = frozenset(['raise', 'ignore', 'call', 'print', 'warn'])
 
-    _profile = {'emptytable':
-                {'ignore': lambda: None,
-                 'warn': lambda: warn('Empty table!'),
-                 'raise': lambda: _raise_ex(TableException),
-                 'call': lambda: None,
-                 'print': lambda: stdout.write('Empty table!')}}
+    def __init__(self):
+        self._profile = {}
+        self._state = {}
+        self._test = {}
 
-    _state = {'emptytable': 'raise'}
-    _test = (('emptytable', lambda t: t.isempty()),)
+    def register(self, errtype, msg, state, test, callback=None,
+                 exception=Exception):
+        """Register an error type
+
+        Paramters
+        ---------
+        errtype : str
+            A name for the error, must be unique.
+        msg : str
+            An error message.
+        state : {'raise', 'ignore', 'call', 'print', 'warn'}
+            The default state.
+        test : function
+            A function to test for error.
+        callback : function, optional
+            A callback function for use with state 'call'
+        exception : Exception, optional
+            An exception to throw in state 'raises'.
+
+        Raises
+        ------
+        KeyError
+            If the errtype already exists
+        KeyError
+            If the state is invalid
+
+        """
+        if errtype in self:
+            raise KeyError("Already registered: %s" % errtype)
+
+        if state not in self._valid_states:
+            raise KeyError("Unknown state: %s" % state)
+
+        self._profile[errtype] = _create_error_states(msg, callback, exception)
+        self._state[errtype] = state
+        self._test[errtype] = test
+
+    def unregister(self, errtype):
+        """Unregister an error type
+
+        Parameters
+        ----------
+        errtype : str
+            The error type to unregister
+
+        Raises
+        ------
+        KeyError
+            If the error type is unknown
+
+        Returns
+        -------
+        dict
+            The profile associated with the error.
+        function
+            The test function associated with the error.
+        str
+            The state at time of unregistering associated with the error
+
+        """
+        if errtype not in self:
+            raise KeyError("Unknown error type: %s" % errtype)
+
+        prof = self._profile.pop(errtype)
+        func = self._test.pop(errtype)
+        state = self._state.pop(errtype)
+
+        return (prof, func, state)
 
     @property
     def state(self):
@@ -30,57 +211,139 @@ class _ErrorProfile(object):
         return self._state
 
     @state.setter
-    def state(self, **kwargs):
+    def state(self, new_state):
         """Update current state"""
-        for errtype, new_state in kwargs.items():
+        if 'all' in new_state:
+            to_update = [(err, new_state['all']) for err in self._state]
+        else:
+            to_update = new_state.items()
+
+        for errtype, new_state in to_update:
             if new_state not in self._valid_states:
                 raise KeyError("Unknown state type: %s" % new_state)
-            self._state[errtype] = new_state
+            if errtype not in self._state:
+                raise KeyError("Unknown error type: %s" % errtype)
 
-    def __getitem__(self, errtype):
-        """Get the current profile for an errortype"""
-        return self._profile[errtype]
+            self._state[errtype] = new_state
 
     def __contains__(self, errtype):
         """Check if an error type exists"""
         return errtype in self._state
 
-    def test(self, item):
-        """Test an item for error"""
-        for errtype, test_func in self._test:
-            if self._state[errtype] != 'ignore' and test_func(item):
-                self._handle_error(errtype)
+    def test(self, item, *args):
+        """Test for an error
 
-    def _handle_error(self, errtype):
+        Parameters
+        ----------
+        item : object
+            An item to test for error
+        *args : list, optional
+            Error types to check, if not provided, all known error types are
+            checked.
+
+        Examples
+        --------
+        >>> from biom import example_table
+        >>> from biom.err import __errprof
+        >>> __errprof.test(example_table, 'empty')
+
+        """
+        if not args:
+            args = self._test.keys()
+
+        for errtype in args:
+            test = self._test.get(errtype, lambda: None)
+            if test(item):
+                return self._handle_error(errtype, item)
+
+    def _handle_error(self, errtype, item):
         """Handle an error"""
         state = self._state[errtype]
-        self._profile[errtype][state]()
+        profile = self._profile[errtype]
+        return profile[state](item)
 
     def setcall(self, errtype, func):
-        """Set a function callback"""
+        """Set a function callback
+
+        Parameters
+        ----------
+        errtype : str
+            The error type to set a callback for.
+        func : function
+            The callback function. This function must accept a single
+            parameter. The result of this function will be returned through
+            the handler.
+
+        Raises
+
+
+        Examples
+        --------
+        >>> from biom.err import __errprof
+        >>> oldcall = __errprof.setcall('empty', lambda item: 123)
+        >>> _ = __errprof.setcall('empty', oldcall)
+
+        """
+        if errtype not in self:
+            raise KeyError("Unknown error type: %s" % errtype)
+
         old_call = self._profile[errtype]['call']
         self._profile[errtype]['call'] = func
         return old_call
 
     def getcall(self, errtype):
-        """Get a function callback"""
+        """Get a function callback
+
+        Parameters
+        ----------
+        errtype : str
+            The error type to get the callback for.
+
+        Raises
+        ------
+        KeyError
+            If the error type is unknown.
+
+        Returns
+        -------
+        function
+            The associated callback function.
+
+        """
+        if errtype not in self:
+            raise KeyError("Unknown error type: %s" % errtype)
+
         return self._profile[errtype]['call']
 
 
-__error_profile = _ErrorProfile()
+__errprof = ErrorProfile()
+__errprof.register('empty', EMPTY, 'ignore', _test_empty,
+                   exception=TableException)
+__errprof.register('obssize', OBSSIZE, 'raise', _test_obssize,
+                   exception=TableException)
+__errprof.register('sampsize', SAMPSIZE, 'raise', _test_sampsize,
+                   exception=TableException)
+__errprof.register('obsdup', OBSDUP, 'raise', _test_obsdup,
+                   exception=TableException)
+__errprof.register('sampdup', SAMPDUP, 'raise', _test_sampdup,
+                   exception=TableException)
+__errprof.register('obsmdsize', OBSMDSIZE, 'raise', _test_obsmdsize,
+                   exception=TableException)
+__errprof.register('sampmdsize', SAMPMDSIZE, 'raise', _test_sampmdsize,
+                   exception=TableException)
 
 
 def geterr():
     """Returns the current error profile state"""
-    return __error_profile.state
+    return __errprof.state.copy()
 
 
-def seterr(all=None, emptytable=None):
+def seterr(**kwargs):
     """How table errors are handled, API based on numpy's seterr
 
-    Parameters
+    Notes
     ----------
-    all : {'ignore', 'warn', 'raise', 'call', 'print', callable}, optional
+    all : {'ignore', 'warn', 'raise', 'call', 'print'}, optional
         Set treatment for all error types
 
         - ignore: Take no action when the exception occurs.
@@ -88,14 +351,6 @@ def seterr(all=None, emptytable=None):
         - raise: Raise a `TableException`.
         - call: Call a function specified using the `seterrcall` function.
         - print: Print a warning directly to ``stdout``.
-        - callable: implies 'call' and will set the callback function for all
-            error types.
-
-    emptytable : {'ignore', 'warn', 'raise', 'call', 'print', callable},
-        optional
-        Treatment of an empty table (e.g., Table.sum(axis='whole') == 0). If a
-        callable is provided, it implies 'call' and will set the callback
-        function.
 
     Returns
     -------
@@ -113,25 +368,23 @@ def seterr(all=None, emptytable=None):
     --------
 
     >>> from numpy import inf
-    >>> from biom.util import seterr
+    >>> from biom.err import seterr
     >>> from biom import example_table
-    >>> example_table.filter(lambda v, i, md: v.sum() > inf, inplace=False)
+    >>> make_empty_f = lambda v, i, md: v.sum() > inf
+    >>> _ = example_table.filter(make_empty_f, inplace=False)
+    >>> old_state = seterr(empty='raise')
+    >>> example_table.filter(make_empty_f, inplace=False)
     Traceback (most recent call last):
     ...
-    biom.exception.TableException: All data was filtered out!
-    >>> seterr(all='ignore')
-    >>> example_table.filter(lambda v, i, md: v.sum() > inf, inplace=False)
+    TableException: Empty table!
+    >>> _ = seterr(**old_state)
 
     """
-    old_state = __error_profile.state.copy()
-    if all is not None:
-        for k in __error_profile.state:
-            __error_profile.state[k] = all
-    elif emptytable is not None:
-        if isinstance(emptytable, types.FunctionType):
-            __error_profile.setcall('emptytable', emptytable)
-        else:
-            __error_profile.state['emptytable'] = emptytable
+    old_state = __errprof.state.copy()
+    if 'all' in kwargs:
+        __errprof.state = {'all': kwargs['all']}
+    else:
+        __errprof.state = kwargs
     return old_state
 
 
@@ -146,7 +399,7 @@ def seterrcall(errtype, func):
     Parameters
     ----------
     errtype : str
-        Must be a valid key in biom.__error_profile
+        Must be a valid key in biom.__errprof
     func : callable
         A function that is called if the given error is encountered and if the
         current error profile indicates 'call' for the error type.
@@ -162,10 +415,10 @@ def seterrcall(errtype, func):
         If the errtype specified is not in the error profile.
 
     """
-    if errtype not in __error_profile:
+    if errtype not in __errprof:
         raise KeyError("Unknown error type: %s" % errtype)
     else:
-        return __error_profile.setcall(errtype, func)
+        return __errprof.setcall(errtype, func)
 
 
 def geterrcall(errtype):
@@ -187,26 +440,37 @@ def geterrcall(errtype):
         If the errtype specified is not in the error profile.
 
     """
-    if errtype not in __error_profile:
+    if errtype not in __errprof:
         raise KeyError("Unknown error type: %s" % errtype)
     else:
-        return __error_profile.getcall(errtype)
+        return __errprof.getcall(errtype)
 
 
-def check_error(table):
+def errcheck(table, *errtypes):
     """Check if there is an error, and respond appropriately
 
     Parameters
     ----------
     table : Table
         The table to check
+    errtypes : vargs of str, if not specified, defaults to all errors
+        Errors to test
+
+    Notes
+    -----
+    If an error type has 'raises' as its state, the exception is raised from
+    here to avoid cluttering the traceback.
 
     Returns
     -------
     dependent on error state and setting
 
     """
-    __error_profile.test(table)
+    ret = __errprof.test(table, *errtypes)
+    if isinstance(ret, Exception):
+        raise ret
+    else:
+        return ret
 
 
 @contextmanager
@@ -221,7 +485,7 @@ def errstate(**kwargs):
 
     Parameters
     ----------
-    kwargs : {emptytable}
+    kwargs : {empty}
         Keyword arguments. The valid error types that are defined. Each keyword
         should have a string or callable for the particular error. If the value
         is a callable, it implies a callback is to be specified for the error
@@ -235,4 +499,4 @@ def errstate(**kwargs):
     """
     old_state = seterr(**kwargs)
     yield
-    seterr(old_state)
+    seterr(**old_state)
---
+++ tests/test_err.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+
+# -----------------------------------------------------------------------------
+# Copyright (c) 2011-2013, The BIOM Format Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from unittest import TestCase, main
+from copy import deepcopy
+
+from biom import example_table, Table
+from biom.exception import TableException
+from biom.err import (_test_empty, _test_obssize, _test_sampsize, _test_obsdup,
+                      _test_sampdup, _test_obsmdsize, _test_sampmdsize,
+                      errstate, geterr, seterr, geterrcall, seterrcall,
+                      errcheck, errstate, __errprof)
+
+
+runtime_ep = __errprof
+runtime_ep_profile = deepcopy(runtime_ep._profile)
+runtime_ep_state = runtime_ep._state.copy()
+runtime_ep_test = runtime_ep._test.copy()
+
+
+class ErrModeTests(TestCase):
+    def setUp(self):
+        self.ex_table = example_table.copy()
+
+    def test_test_empty(self):
+        self.assertTrue(_test_empty(Table([], [], [])))
+        self.assertFalse(_test_empty(self.ex_table))
+
+    def test_test_obssize(self):
+        self.assertFalse(_test_obssize(self.ex_table))
+        self.ex_table._observation_ids = self.ex_table._observation_ids[:-1]
+        self.assertTrue(_test_obssize(self.ex_table))
+
+    def test_test_sampsize(self):
+        self.assertFalse(_test_sampsize(self.ex_table))
+        self.ex_table._sample_ids = self.ex_table._sample_ids[:-1]
+        self.assertTrue(_test_sampsize(self.ex_table))
+
+    def test_test_obsdup(self):
+        self.assertFalse(_test_obsdup(self.ex_table))
+        self.ex_table._observation_ids[0] = self.ex_table._observation_ids[1]
+        self.assertTrue(_test_obsdup(self.ex_table))
+
+    def test_test_sampdup(self):
+        self.assertFalse(_test_sampdup(self.ex_table))
+        self.ex_table._sample_ids[0] = self.ex_table._sample_ids[1]
+        self.assertTrue(_test_sampdup(self.ex_table))
+
+    def test_test_obsmdsize(self):
+        self.assertFalse(_test_obsdup(self.ex_table))
+        self.ex_table._observation_metadata = \
+            self.ex_table._observation_metadata[:-1]
+        self.assertTrue(_test_obsmdsize(self.ex_table))
+
+    def test_test_sampmdsize(self):
+        self.assertFalse(_test_sampdup(self.ex_table))
+        self.ex_table._sample_metadata = \
+            self.ex_table._sample_metadata[:-1]
+        self.assertTrue(_test_sampmdsize(self.ex_table))
+
+
+class ErrorProfileTests(TestCase):
+    def setUp(self):
+        self.ex_table = example_table.copy()
+        self.ep = runtime_ep
+        self.ep.state = {'all': 'raise'}
+
+    def tearDown(self):
+        self.ep._profile = deepcopy(runtime_ep_profile.copy())
+        self.ep._state = runtime_ep_state.copy()
+        self.ep._test = runtime_ep_test.copy()
+
+    def test_test(self):
+        self.ep.test(self.ex_table)
+        self.ep.test(self.ex_table, 'empty')
+        self.ep.test(self.ex_table, 'empty', 'obssize')
+
+        self.ex_table._observation_ids = self.ex_table._observation_ids[:-1]
+        self.ep.test(self.ex_table, 'empty')
+        self.assertTrue(isinstance(self.ep.test(self.ex_table, 'obssize'),
+                                   TableException))
+
+    def test_state(self):
+        self.ep.state = {'all': 'ignore'}
+        self.assertEqual(set(self.ep._state.values()), set(['ignore']))
+        self.ep.state = {'empty': 'call'}
+        self.assertEqual(set(self.ep._state.values()), set(['ignore', 'call']))
+        self.assertEqual(self.ep.state['empty'], 'call')
+
+        with self.assertRaises(KeyError):
+            self.ep.state = {'empty': 'missing'}
+
+        with self.assertRaises(KeyError):
+            self.ep.state = {'emptyasdasd': 'ignore'}
+
+    def test_contains(self):
+        self.assertTrue('empty' in self.ep)
+        self.assertFalse('emptyfoo' in self.ep)
+
+    def test_handle_error(self):
+        def callback(foo):
+            return 10
+
+        self.ep.setcall('empty', callback)
+
+        self.assertTrue(isinstance(self.ep._handle_error('empty', None),
+                                   TableException))
+        self.ep.state = {'empty': 'call'}
+        self.assertEqual(self.ep._handle_error('empty', None), 10)
+
+    def test_setcall(self):
+        def callback(foo):
+            return 10
+
+        self.assertEqual(self.ep._profile['empty']['call'](None), None)
+        self.ep.setcall('empty', callback)
+        self.assertEqual(self.ep._profile['empty']['call'](None), 10)
+
+        with self.assertRaises(KeyError):
+            self.ep.setcall('emptyfoo', callback)
+
+    def test_getcall(self):
+        def callback(foo):
+            return 10
+        self.ep.setcall('empty', callback)
+        self.assertEqual(self.ep.getcall('empty'), callback)
+
+        with self.assertRaises(KeyError):
+            self.ep.getcall('emptyfoo')
+
+    def test_register_unregister(self):
+        cb = lambda x: 123
+        test = lambda x: x == 5
+
+        self.ep.register('foo', 'bar', 'ignore', test, callback=cb)
+        self.assertTrue('foo' in self.ep)
+        self.ep.state = {'foo': 'call'}
+        self.assertEqual(self.ep._handle_error('foo', None), 123)
+
+        foo_prof = self.ep._profile['foo'].copy()
+        prof, func, state = self.ep.unregister('foo')
+
+        self.assertEqual(func, test)
+        self.assertEqual(state, 'call')
+        self.assertEqual(prof, foo_prof)
+
+        with self.assertRaises(KeyError):
+            self.ep.register('empty', 1, 2, lambda: None)
+
+        with self.assertRaises(KeyError):
+            self.ep.register('foo', 'missing', 2, lambda: None)
+
+        with self.assertRaises(KeyError):
+            self.ep.unregister('non_existant')
+
+
+class SupportTests(TestCase):
+    def setUp(self):
+        self.ex_table = example_table.copy()
+
+    def test_geterr(self):
+        state = geterr()
+        self.assertEqual(state, runtime_ep._state)
+        old = seterr(all='call')
+        self.assertNotEqual(geterr(), state)
+        seterr(**old)
+
+    def test_seterr(self):
+        existing = seterr(empty='warn')
+        self.assertEqual(runtime_ep._state['empty'], 'warn')
+        self.assertNotEqual(runtime_ep._state['empty'], existing)
+        seterr(empty=existing['empty'])
+        self.assertNotEqual(runtime_ep._state['empty'], 'warn')
+        self.assertEqual(runtime_ep._state, existing)
+
+    def test_geterrcall(self):
+        exp = runtime_ep._profile['sampsize']['call']
+        obs = geterrcall('sampsize')
+        self.assertEqual(obs, exp)
+
+        with self.assertRaises(KeyError):
+            geterrcall('asdasd')
+
+    def test_seterrcall(self):
+        def foo(x):
+            return 10
+
+        seterrcall('sampmdsize', foo)
+        obs = geterrcall('sampmdsize')
+        self.assertEqual(obs, foo)
+
+    def test_errcheck(self):
+        self.assertEqual(errcheck(self.ex_table), None)
+        self.ex_table._sample_ids = self.ex_table._sample_ids[:-1]
+        with self.assertRaises(TableException):
+            errcheck(self.ex_table)
+
+    def test_errstate(self):
+        def foo(item):
+            return "the callback called"
+
+        table = Table([], [], [])
+        seterrcall('empty', foo)
+        self.assertNotEqual(geterr()['empty'], 'call')
+        with errstate(empty='call'):
+            result = errcheck(table)
+        self.assertEqual(result, "the callback called")
+        self.assertNotEqual(geterr()['empty'], 'call')
+
+if __name__ == '__main__':
+    main()
