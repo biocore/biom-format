eb2d29363e7798b602998cb0e763025f40db4656
Daniel McDonald <mcdonadt@colorado.edu>
2016-09-26 08:48:01 -0700

    BUG: test for  explicitly

--- biom/table.py
+++ biom/table.py
@@ -256,6 +256,8 @@ def vlen_list_of_str_formatter(grp, header, md, compression):
     for m in md:
         if m[header] is None:
             iterable_checks.append(True)
+        elif isinstance(m.get(header), str):
+            iterable_checks.append(False)
         else:
             iterable_checks.append(
                 isinstance(m.get(header, []), Iterable))
@@ -265,7 +267,8 @@ def vlen_list_of_str_formatter(grp, header, md, compression):
         raise TypeError(
             "Category %s not formatted correctly. Did you pass"
             " --process-obs-metadata taxonomy when converting "
-            " from tsv?")
+            " from tsv? Please see Table.to_hdf5 docstring for"
+            " more information")
 
     max_list_len = max(lengths)
     shape = (len(md), max_list_len)
