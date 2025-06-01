e71a892e05a2eb6056d8f042de79d15cb9055408
Qiyun Zhu <qiyunzhu@gmail.com>
2025-05-31 19:40:40 -0700

    dropping python 3.9 support

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ classes = """
     Programming Language :: Python
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3 :: Only
-    Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
     Programming Language :: Python :: 3.11
     Programming Language :: Python :: 3.12
